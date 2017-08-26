a87f31f3ed558e3981f8af737151bf52d1f41491
agustin <agustinterlaak@gmail.com>
2017-08-26 04:13:52 -0300

    archivo .sql

--- db/base de datos Sesiones.sql
+++ db/base de datos Sesiones.sql
@@ -1,9 +1,43 @@
-﻿Create Table Usuario
+CREATE Table Usuario
 (Id serial Primary key,
 Usuario varchar(15) not null,
 Contrasena varchar(12) not null,
 Email varchar(30) not null,
 TipoUsuario varchar(15) not null
 );
+INSERT INTO `Usuario` (`Id`, `Usuario`, `Contrasena`, `Email`, `TipoUsuario`) VALUES (NULL, 'test', 'test', 'asdasd@asd', 'admin');
+INSERT INTO `Usuario` (`Id`, `Usuario`, `Contrasena`, `Email`, `TipoUsuario`) VALUES (NULL, 'test2', 'asd', 'asdasd@asd', 'user');
 
-select * from Usuario;
+Create Table Factura
+(Id serial Primary key,
+Tipo varchar(2) not null,
+FormaPago varchar(50) not null,
+Persona varchar(50) not null, /*id de persona */ 
+NroComprobante varchar(20) not null,
+Fecha date not null,
+Iva float not null,
+Descuento float not null
+);
+
+INSERT INTO `Factura` (`Id`, `Tipo`, `FormaPago`, `Persona`, `NroComprobante`,`fecha`,`Iva`,`Descuento`) VALUES (NULL, 'A', 'Contado', '1', '1234', '2017/08/30', '3', '0');
+
+Create Table Producto
+(Id serial Primary key,
+Stock int not null,
+NombreProducto varchar(200) not null,
+CostoProducto float(8,2) not null
+);
+
+INSERT INTO `Producto` (`Id`, `Stock`, `NombreProducto`, `CostoProducto`) VALUES (NULL, '10', 'Coca', '10.23');
+
+Create Table Persona
+(Id serial Primary key,
+TipoPersona int not null,
+Nombre varchar(70) not null,
+Telefono int not null,
+Direccion varchar(100) not null,
+Cuit int not null
+);
+
+INSERT INTO `Persona` (`Id`, `TipoPersona`, `Nombre`, `Telefono`, `Direccion`, `Cuit`) VALUES(NULL, '1', 'Fulanito S.A', '3752504440', 'alguna calle 123', '30707039902');
+
